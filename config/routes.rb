5b53c0c8b8c4f02fb5dfc61edc5cb08563102781
Jędrzej Domański <jedrek.domanski@gmail.com>
2023-10-17 22:06:25 +0100

    Fix API

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   get '/status', to: proc { [200, {}, ['OK']] }
 
-  namespace :api do
-    namespace :v1 do
-      mount Services::Api => '/'
-    end
+  scope :api do
+    mount Services::Api => '/'
   end
 end
